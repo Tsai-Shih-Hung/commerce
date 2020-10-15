3ca50522a3e15be0a0090c4083b3de2557a0a9e6
Tsai-Shih-Hung <brian9229222@gmail.com>
2020-10-15 19:02:08 +0800

    'axios_problem'

---
+++ commerce/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '@/components/HelloWorld';
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    routes:[
+        {
+        name:'首頁',
+        path:'/',
+        component:Home,
+    }
+    ]
+})
